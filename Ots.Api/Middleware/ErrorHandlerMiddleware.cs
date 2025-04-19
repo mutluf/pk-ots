fff91567105a61cb81e5dd9795582dfad17ec3b1
ben10 <ben10>
2025-04-19 13:40:44 +0300

    feat: Enhance error logging in ErrorHandlerMiddleware for better exception tracking

--- Ots.Api/Middleware/ErrorHandlerMiddleware.cs
+++ Ots.Api/Middleware/ErrorHandlerMiddleware.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 namespace Ots.Api.Middleware;
 
 public class ErrorHandlerMiddleware
@@ -16,7 +18,15 @@ public class ErrorHandlerMiddleware
             await next(context);
         }
         catch (Exception ex)
-        {
+        {             
+            Log.Fatal($"Path: {context.Request.Path} {Environment.NewLine}" +
+                      $"Method: {context.Request.Method} {Environment.NewLine}" +
+                      $"QueryString: {context.Request.QueryString} {Environment.NewLine}" +
+                      $"StatusCode: {context.Response.StatusCode} {Environment.NewLine}" +
+                      $"Exception: {ex.Message}");
+
+            Log.Fatal(ex, "An unhandled exception occurred while processing the request.");
+
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = 500;
             await context.Response.WriteAsync(ex.Message);
